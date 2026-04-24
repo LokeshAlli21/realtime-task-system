2d1068b9b5b228776baf42ed838a743f703d7749
Lokesh Alli <lokeshalli1807@gmail.com>
2026-04-24 16:39:24 +0530

    add FRONTEND_ORIGIN_URL

--- backend/src/env/env.js
+++ backend/src/env/env.js
@@ -3,10 +3,11 @@ dotenv.config()
 
 const config = {
     port: process.env.PORT,
-    frontendUrl: process.env.FRONTEND_URL,
+    frontendUrl: process.env.FRONTEND_ORIGIN_URL,
     databaseUrl: process.env.DATABASE_URL,
     JWTSecret: process.env.JWT_SECRET,
     JWTExpiresIn: process.env.JWT_EXPIRES_IN,
 }
+console.log(config)
 
 export default config
